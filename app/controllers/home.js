a9cbb4489af63df18ef214b1ca7a5a8d011858e4
NicholasMaestrello <rainickmr@gmail.com>
2017-08-13 20:42:11 -0300

    style

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,8 @@
 module.exports.index = function (app, req, res) {
-    res.render('home/index')
+    let connection = app.config.dbConnection()
+    let noticiasModel = new app.app.models.NoticiasDAO(connection)
+
+    noticiasModel.get5UltimasNoticias(function (err, result) {
+        res.render('home/index', {noticias: result})
+    })
 }
